6a47d951bd1cc92094449e4d9c85e1ad1877d522
WPBooster <WPBooster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-08 13:07:29 +0000

    skrypt odbierający cache zwraca odpowiedź
    git-svn-id: https://plugins.svn.wordpress.org/seo-wpbooster/trunk@214931 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbupdcache.php
+++ wpbupdcache.php
@@ -4,14 +4,20 @@ $cacheser = base64_decode($_GET['cache']);
 $cache = unserialize($cacheser);
 if ($cache) {
 	if(!get_option('wpbooster_cache')) {
+		$response['cacheexist'] = false;
 		add_option('wpbooster_cache',$cacheser);
 	}
 	else {
+		$response['cacheexist'] = true;
+		$update_index = 0;
 		$update_tab = unserialize(get_option('wpbooster_cache'));
 		foreach ($cache as $cache_key => $cache_val) {
 			$update_tab[$cache_key] = $cache_val;
+			$update_index++;
 		}
 		update_option('wpbooster_cache', serialize($update_tab));
+		$response['cachecounter'] = $update_index;
 	}
+	echo base64_encode(serialize($response));
 }
 ?>
