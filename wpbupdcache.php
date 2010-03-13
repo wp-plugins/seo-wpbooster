6ca525eee01b9b427e7369d5e401a17fa7f4128f
WPBooster <WPBooster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-13 12:06:02 +0000

    dodane sprawdzanie cache'u na blogu
    git-svn-id: https://plugins.svn.wordpress.org/seo-wpbooster/trunk@216899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbupdcache.php
+++ wpbupdcache.php
@@ -1,23 +1,33 @@
 <?php
 include('../../../wp-config.php');
-$cacheser = base64_decode($_GET['cache']);
-$cache = unserialize($cacheser);
-if ($cache) {
-	if(!get_option('wpbooster_cache')) {
-		$response['cacheexist'] = false;
-		add_option('wpbooster_cache',$cacheser);
-	}
-	else {
-		$response['cacheexist'] = true;
-		$update_index = 0;
-		$update_tab = unserialize(get_option('wpbooster_cache'));
-		foreach ($cache as $cache_key => $cache_val) {
-			$update_tab[$cache_key] = $cache_val;
-			$update_index++;
+if ($_GET['cache']) {
+	$cacheser = base64_decode($_GET['cache']);
+	$cache = unserialize($cacheser);
+	if ($cache) {
+		if(!get_option('wpbooster_cache')) {
+			$response['cacheexist'] = false;
+			add_option('wpbooster_cache',$cacheser);
 		}
-		update_option('wpbooster_cache', serialize($update_tab));
-		$response['cachecounter'] = $update_index;
+		else {
+			$response['cacheexist'] = true;
+			$update_index = 0;
+			$update_tab = unserialize(get_option('wpbooster_cache'));
+			foreach ($cache as $cache_key => $cache_val) {
+				$update_tab[$cache_key] = $cache_val;
+				$update_index++;
+			}
+			update_option('wpbooster_cache', serialize($update_tab));
+			$response['cachecounter'] = $update_index;
+		}
+		echo base64_encode(serialize($response));
 	}
-	echo base64_encode(serialize($response));
+}
+elseif ($_GET['check_cache']) {
+	$cache_tab = unserialize(get_option('wpbooster_cache'));
+	print_r($cache_tab);
+}
+elseif ($_GET['check_cache_encode']) {
+	$cache_ser = get_option('wpbooster_cache');
+	echo '|wpbooster|'.base64_encode($cache_ser).'|wpbooster|';
 }
 ?>
