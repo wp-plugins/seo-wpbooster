861d25b63541f17bc8bb8eee9871ecaebad625eb
WPBooster <WPBooster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-07 08:56:44 +0000

    obsługa języka angielskiego
    git-svn-id: https://plugins.svn.wordpress.org/seo-wpbooster/trunk@238224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbadmin.php
+++ wpbadmin.php
@@ -1,4 +1,88 @@
 <?
+
+$lang_tab_pl['save_view'] = 'Zapisano zmianę ustawień wyświetlania linków z systemu';
+$lang_tab_pl['blog_add_true'] = 'Blog został dodany do systemu';
+$lang_tab_pl['blog_in_system'] = 'Blog znajduje się w systemie WPBooster.';
+$lang_tab_pl['links_auto_posts'] = 'Linki z systemu pojawiają się teraz automatycznie w każdym poście.';
+$lang_tab_pl['manual_add_link'] = 'Włącz ręczne dodawanie linków do szablonu';
+$lang_tab_pl['auto_add_link'] = 'Włącz automatyczne dodawanie linków do każdego postu';
+$lang_tab_pl['view_links_manual'] = 'Linki z systemu możesz teraz ręcznie umieścić w dowolnym miejscu w szablonie.';
+$lang_tab_pl['wpbooster_link_true'] = 'Jeśli chcesz żeby linki z systemu WPbooster ukazały się na Twoim blogu wklej w dowolnym miejscu szablonu (prócz pętli) kod:<br />&lt;?php echo wpb_getlinks(); ?&gt;';
+$lang_tab_pl['div_info1'] = 'Kontener, w którym znajdują się linki z systemu ma id = "';
+$lang_tab_pl['div_info2'] = 'Możesz zdefiniować jego wygląd w arkuszu stylu pisząc:';
+
+
+$lang_tab_en['save_view'] = 'Zapisano zmianę ustawień wyświetlania linków z systemu';
+$lang_tab_en['blog_add_true'] = 'Blog został dodany do systemu';
+$lang_tab_en['blog_in_system'] = 'Blog znajduje się w systemie WPBooster.';
+$lang_tab_en['links_auto_posts'] = 'Linki z systemu pojawiają się teraz automatycznie w każdym poście.';
+$lang_tab_en['manual_add_link'] = 'Włącz ręczne dodawanie linków do szablonu';
+$lang_tab_en['auto_add_link'] = 'Włącz automatyczne dodawanie linków do każdego postu';
+$lang_tab_en['view_links_manual'] = 'Linki z systemu możesz teraz ręcznie umieścić w dowolnym miejscu w szablonie.';
+$lang_tab_en['wpbooster_link_true'] = 'Jeśli chcesz żeby linki z systemu WPbooster ukazały się na Twoim blogu wklej w dowolnym miejscu szablonu (prócz pętli) kod:<br />&lt;?php echo wpb_getlinks(); ?&gt;';
+$lang_tab_en['div_info1'] = 'Kontener, w którym znajdują się linki z systemu ma id = "';
+$lang_tab_en['div_info2'] = 'Możesz zdefiniować jego wygląd w arkuszu stylu pisząc:';
+
+
+
+$lang_tab_pl['not_active_yet'] = 'Plugin <b>WPBooster</b> jest nieaktywny. Musisz go najpierw skonfigurować!';
+$lang_tab_en['not_active_yet'] = '<b>WPBooster</b> plugin is not active yet. You have to set it up first!';
+
+$lang_tab_pl['step_two'] = 'Krok 2 z 2<br />Dane użytkownika i bloga';
+$lang_tab_en['step_two'] = 'Step 2 of 2<br />User and blog details';
+
+$lang_tab_pl['no_blog'] = 'Wypełnij poniższe pola nazwą użytkownika i hasłem z <a href="http://wpbooster.pl/" target="_blank">WPBooster.pl</a>. Jeśli nie masz jeszcze konta, <a href="http://wpbooster.pl/wp-login.php?action=register" target="_blank">zarejestruj się</a>.';
+$lang_tab_en['no_blog'] = 'Fill out forms below with <a href="http://wpbooster.com/" target="_blank">WPBooster.com</a> username and password. If you don\'t have account, <a href="http://wpbooster.com/wp-login.php?action=register" target="_blank">register one here</a>.';
+
+$lang_tab_pl['username'] = 'Użytkownik: ';
+$lang_tab_en['username'] = 'Username: ';
+
+$lang_tab_pl['password'] = 'Hasło: ';
+$lang_tab_en['password'] = 'Password: ';
+
+$lang_tab_pl['please_select'] = 'Proszę wybrać kategorię';
+$lang_tab_en['please_select'] = 'Please select a category';
+
+$lang_tab_pl['select_category'] = 'Wybierz kategorię';
+$lang_tab_en['select_category'] = 'Select category';
+
+$lang_tab_pl['blog_add'] = 'Aktywuj wtyczkę';
+$lang_tab_en['blog_add'] = 'Activate plugin';
+
+$lang_tab_pl['message_exist_domain'] = 'Inny użytkownik dodał tą domenę do systemu, jeśli Ty jesteś jej właścicielem, <a href="http://wpbooster.pl/kontakt" target="_blank">skontaktuj się z nami</a>.';
+$lang_tab_en['message_exist_domain'] = 'This domain is already connected by another member, if youre owner of this blog, please, <a href="http://wpbooster.com/contact" target="_blank">let us know</a>.';
+
+$lang_tab_pl['error_username'] = 'Nazwa użytkownika lub hasło jest nieprawidłowe! Jeśli dodałeś bloga do systemu
+<a href="http://wpbooster.com/" target="_blank">wpbooster.com</a>
+<a href="options-general.php?page=seo-wpbooster/wpbadmin.php&lang=en">zmień język na angielski</a>
+i wpisz jeszcze raz nazwę użytkownika i hasło w formularzu poniżej.';
+$lang_tab_en['error_username'] = 'Username or password is invalid! If you have added your blog to the
+<a href="http://wpbooster.pl/" target="_blank">wpbooster.pl</a> system
+<a href="options-general.php?page=seo-wpbooster/wpbadmin.php&lang=pl">change language to Polish</a>
+and once again enter your username and password in the form below.';
+
+
+
+$lang_tab_pl['error_exchange'] = 'Błąd wymiany danych pomiędzy wtyczką i systemem.';
+$lang_tab_en['error_exchange'] = 'Error with data exchange between plugin and the system.';
+
+$lang_tab_pl['fopen_off'] = 'Niestety na serwerze allow_url_fopen jest ustawione na off, a cURL nie działa.';
+$lang_tab_en['fopen_off'] = 'Unfortunately, allow_url_fopen on your server is set to off, and cURL doesn\'t work.';
+
+$lang_tab_pl['please_contact'] = 'Prosimy o kontakt z administracją systemu WPbooster. Prosimy do wiadomości załączyć kopię lub screen tej strony.<br />Przepraszamy za utrudnienia.';
+$lang_tab_en['please_contact'] = 'Please contact WPBooster system administrator and attach a copy of this message or screen of this page.<br />Sorry for the inconvenience.';
+
+
+if ($_GET['lang']) {
+	update_option('wpbooster_lang', $_GET['lang']);
+}
+
+if (get_option('wpbooster_lang') == 'pl') $lang_tab = $lang_tab_pl;
+elseif (get_option('wpbooster_lang') == 'en') $lang_tab = $lang_tab_en;
+else $lang_tab = $lang_tab_en;
+
+
+
 add_action( 'admin_menu', 'submenu_admina_wpbooster' );
 function wpb_file_get_contents($url) {
 	if (ini_get('allow_url_fopen') == 'on') {
@@ -16,80 +100,107 @@ function wpb_file_get_contents($url) {
 }
 function submenu_admina_wpbooster() {add_options_page('WPBOOSTER', 'WPBOOSTER', 10, __FILE__, 'konfigurator_wtyczki_wpbooster');}
 function konfigurator_wtyczki_wpbooster() {
+	global $lang_tab;
 	global $wpdb;
 	echo '<div class="wrap">';
 	if ($_POST['mode'] == 'linkinloop') {
-		echo '<div id="message" class="updated fade"><p>Zapisano zmianę ustawień wyświetlania linków z systemu</p></div>';
+		echo '<div id="message" class="updated fade"><p>'.$lang_tab['save_view'].'</p></div>';
 		update_option('wpbooster_linkinloop', $_POST['linkinloopval']);
 		$data_tab = array(get_bloginfo('url'), $_POST['linkinloopval']);
 		$data = urlencode(base64_encode(serialize($data_tab)));
 		wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/linkinloop.php?data='.$data);
 	}
 	elseif ($_POST['mode'] == 'addblog') {
-		if ($_POST['category'] == '0') echo '<div id="message" class="updated fade"><p>Proszę wybrać kategorię</p></div>';
+		if ($_POST['category'] == '0') echo '<div id="message" class="updated fade"><p>'.$lang_tab['please_select'].'</p></div>';
 		else {
 			$data_tab = array($_POST['user'], $_POST['pass'], get_bloginfo('url'), $_POST['category']);
 			$data = urlencode(base64_encode(serialize($data_tab)));
 			$addvalid = wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/addblog.php?data='.$data);
-			if ($addvalid == 'yes') echo '<div id="message" class="updated fade"><p>Blog został dodany do systemu</p></div>';
-			elseif ($addvalid == 'no') echo '<div id="message" class="updated fade"><p>Podane dane użytkownika są nieprawidłowe!</p></div>';
-			elseif ($addvalid == 'isset') echo '<div id="message" class="updated fade"><p>Inny użytkownik dodał tą domenę do systemu. Prosimy o kontakt (formularz na stronie wpbooster.pl)</p></div>';
+			if ($addvalid == 'yes') echo '<div id="message" class="updated fade"><p>'.$lang_tab['blog_add_true'].'</p></div>';
+			elseif ($addvalid == 'no') echo '<div id="message" class="updated fade"><p>'.$lang_tab['error_username'].'</p></div>';
+			elseif ($addvalid == 'isset') echo '<div id="message" class="updated fade"><p>'.$lang_tab['message_exist_domain'].'</p></div>';
 		}
 	}
+
 	if ( (ini_get('allow_url_fopen') == 'on') || (function_exists('curl_init')) ) {
 		$zakodowany_url = urlencode(base64_encode(get_bloginfo('url')));
 		$issetblog = wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/issetblog.php?blogurl='.$zakodowany_url);
 		if ($issetblog == 'yes') {
-			echo '<br />Blog znajduje się w systemie WPBooster.<br /><br />';
+			echo '<br />'.$lang_tab['blog_in_system'].'<br /><br />';
 			if (get_option('wpbooster_linkinloop') == '1') {
-				echo 'Linki z systemu pojawiają się teraz automatycznie w każdym poście.
+				echo $lang_tab['links_auto_posts'].'
 				<form name="put_definiction" method="post" action="options-general.php?page=seo-wpbooster/wpbadmin.php">
 				<input type="hidden" value="linkinloop" name="mode" />
 				<input type="hidden" value="0" name="linkinloopval" />
-				<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value=" &nbsp; Wlacz reczne dodawanie linkow do szablonu &nbsp; " name="mode_log"/>
+				<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value=" &nbsp; '.$lang_tab['manual_add_link'].' &nbsp; " name="mode_log"/>
 				</form>';
 			}
 			else {
-				echo 'Linki z systemu możesz teraz ręcznie umieścić w dowolnym miejscu w szablonie.
+				echo $lang_tab['view_links_manual'].'
 				<form name="put_definiction" method="post" action="options-general.php?page=seo-wpbooster/wpbadmin.php">
 				<input type="hidden" value="linkinloop" name="mode" />
 				<input type="hidden" value="1" name="linkinloopval" />
-				<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value=" &nbsp; Wlacz automatyczne dodawanie linkow do kazdego postu &nbsp; " name="mode_log"/>
+				<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value="'.$lang_tab['auto_add_link'].'" name="mode_log"/>
 				</form>
-				Jeśli chcesz żeby linki z systemu wpbooster ukazały się na Twoim blogu wklej w dowolnym miejscu szablonu (prócz pętli) kod:<br />&lt;?php echo wpb_getlinks(); ?&gt;<br />';
+				'.$lang_tab['wpbooster_link_true'].'<br />';
 			}
 			$divname = wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/divname.php?blogurl='.$zakodowany_url);
-			echo '<br>Kontener w którym znajdują się linki z systemu ma id = "'.$divname.'"
-			<br>Możesz zdefiniować jego wygląd w arkuszu stylu pisząc:<br>div#'.$divname.' {<br />...<br />}<br />';
+			echo '<br />'.$lang_tab['div_info1'].$divname.'"<br />'.$lang_tab['div_info2'].'<br />div#'.$divname.' {<br />...<br />}<br />';
 		}
 		elseif ($issetblog == 'no') {
-			echo '<br />Bloga nie ma w systemie WPBooster. Podaj swoją nazwę użytkownika i hasło aby dodać bloga do systemu.<br />
-			<form name="put_definiction" method="post" action="options-general.php?page=seo-wpbooster/wpbadmin.php">
-			<input type="text" style="border: 1px solid rgb(0, 0, 0); width: 175px; background-color: rgb(247, 247, 247); margin-top: 5px;" value="" name="user" />
-			(nazwa uzytkownika)<br/>
-			<input type="password" style="border: 1px solid rgb(0, 0, 0); width: 175px; background-color: rgb(247, 247, 247); margin-top: 5px;" value="" name="pass" />
-			(haslo)<br/>';
-			$listakategorii = unserialize(base64_decode(wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/listcategory.php')));
-			echo '<br /><select name="category"><option value="0" selected="selected">Wybierz kategorię bloga</option>';
-			foreach ($listakategorii as $onecat) {
-			    echo '<option value="'.$onecat[0].'">'.$onecat[1].'</option>';
+			echo '<div id="message" class="updated fade">'.$lang_tab['not_active_yet'].'</p></div>';
+			if (!get_option('wpbooster_lang')) {
+				echo '<div style="width: 45%; float: left; border-right: 1px solid #000;">
+					<p style="padding-left: 20px; font-size: 20px;">Step 1 of 2<br />Choose your language</p>
+					<p style="padding-left: 50px;">
+						<a href="options-general.php?page=seo-wpbooster/wpbadmin.php&lang=en"><img src="../wp-content/plugins/seo-wpbooster/en.jpg" alt="en" /></a>
+					</p>
+				</div>
+				<div style="width: 45%; float: left;">
+					<p style="padding-left: 20px; font-size: 20px;">Krok 1 z 2<br />Wybierz język</p>
+					<p style="padding-left: 50px;">
+						<a href="options-general.php?page=seo-wpbooster/wpbadmin.php&lang=pl"><img src="../wp-content/plugins/seo-wpbooster/pl.jpg" alt="pl" /></a>
+					</p>
+				</div><br style="clear: both;" />';
 			}
-			echo '</select><br /><br />
-			<input type="hidden" value="addblog" name="mode" />
-			<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value=" &nbsp; Dodaj blog do systemu &nbsp; " name="mode_log"/>
-			</form>';
+			else {
+				echo '<p style="padding-left: 20px; font-size: 20px;">'.$lang_tab['step_two'].'</p>
+				<br />'.$lang_tab['no_blog'].'<br />
+				<form name="put_definiction" method="post" action="options-general.php?page=seo-wpbooster/wpbadmin.php">
+					<table>
+						<tr><td>
+							'.$lang_tab['username'].'
+						</td><td>
+							<input type="text" style="border: 1px solid rgb(0, 0, 0); width: 175px; background-color: rgb(247, 247, 247); margin-top: 5px;" value="" name="user" />
+						</td></tr><tr><td>
+							'.$lang_tab['password'].'
+						</td><td>
+							<input type="password" style="border: 1px solid rgb(0, 0, 0); width: 175px; background-color: rgb(247, 247, 247); margin-top: 5px;" value="" name="pass" />
+						</td></tr><tr><td>
+							Blog:
+						</td><td>
+							'; $listakategorii = unserialize(base64_decode(wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/listcategory.php')));
+							echo '<select name="category"><option value="0" selected="selected">'.$lang_tab['select_category'].'</option>';
+							foreach ($listakategorii as $onecat) {
+							    echo '<option value="'.$onecat[0].'">'.$onecat[1].'</option>';
+							}
+							echo '</select>
+						</td></tr>
+					</table>
+					<input type="hidden" value="addblog" name="mode" />
+					<input type="submit" style="border: 1px solid rgb(0, 0, 0); cursor: pointer; background-color: rgb(247, 247, 247); margin-top: 5px;" value=" &nbsp; '.$lang_tab['blog_add'].' &nbsp; " name="mode_log"/>
+				</form>';
+			}
 		}
 		else {
-			echo '<br />Błąd wymiany danych pomiędzy wtyczką i systemem.<br />
+			echo '<br />'.$lang_tab['error_exchange'].'<br />
 			|'.$issetblog.'|<br />
-			Prosimy o kontakt z administracją systemu wpbooster. Prosimy do wiadomości załączyć kopię lub screen tej strony.<br />
-			Przepraszamy za utrudnienia.<br />';
+			'.$lang_tab['please_contact'].'<br />';
 		}
 	}
 	else {
-		echo '<br />Niestety na serwerze allow_url_fopen jest ustawione na off, a cURL nie działa.<br />
-		Prosimy o kontakt z administracją systemu wpbooster. Prosimy do wiadomości załączyć kopię lub screen tej strony.<br />
-		Przepraszamy za utrudnienia.<br />';
+		echo '<br />'.$lang_tab['fopen_off'].'<br />
+		'.$lang_tab['please_contact'].'<br />';
 	}
 	echo '</div>';
 }
