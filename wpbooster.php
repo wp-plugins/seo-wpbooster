0f042f9229abea8a9d8edd6fd783b56738020038
WPBooster <WPBooster@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-22 12:36:14 +0000

    zmiana serwera przy wyborze języka
    git-svn-id: https://plugins.svn.wordpress.org/seo-wpbooster/trunk@244612 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpbooster.php
+++ wpbooster.php
@@ -7,31 +7,26 @@ Author: WPBooster Development Team
 Version: 1.5
 Author URI: http://wpbooster.pl/
 */
-
 include('wpbadmin.php');
-
-if(!get_option('wpbooster_linkinloop')) {
+if ( (!get_option('wpbooster_linkinloop')) && (get_option('wpbooster_lang')) ) {
+	if (get_option('wpbooster_lang') == 'pl') $domain = 'pl';
+	else (get_option('wpbooster_lang') == 'en') $domain = 'com';
 	$data = urlencode(base64_encode(get_bloginfo('url')));
-	$link_in_loop = wpb_file_get_contents('http://wpbooster.pl/swlserwer/wpadmin/linkinloop_check.php?blogurl='.$data);
+	$link_in_loop = wpb_file_get_contents('http://wpbooster.'.$domain.'/swlserwer/wpadmin/linkinloop_check.php?blogurl='.$data);
 	add_option('wpbooster_linkinloop', $link_in_loop);
 }
-
 function links_from_wpb() {
 	global $wp_query;
 	$PostId = $wp_query->post->ID;
 	$cache_tab = unserialize(get_option('wpbooster_cache'));
 	return $cache_tab[$PostId];
 }
-
 function wpb_getlinks() {
     if ( (get_option('wpbooster_linkinloop') == '0') && (is_single()) ) return links_from_wpb();
 }
-
 function link_in_loop($content) {
     if ( (get_option('wpbooster_linkinloop') == '1') && (is_single()) ) $dodane = links_from_wpb();
     return $content.$dodane;
 }
-
 add_filter('the_content', 'link_in_loop');
-
 ?>
